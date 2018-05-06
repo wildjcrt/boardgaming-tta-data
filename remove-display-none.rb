62b6981c3a4b3ac186f2391ee88ea60a1c04d386
wildjcrt <wildjcrt@yahoo.com.tw>
2018-05-06 10:20:12 +0800

    Add new-tta in remove-display-none.rb

--- remove-display-none.rb
+++ remove-display-none.rb
@@ -1,4 +1,4 @@
-Dir['tta/*'].each do |filename|
+Dir['tta/*', 'new-tta/*'].each do |filename|
   html = File.read(filename).encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
   new_html = html.gsub(/ style="display:none;"/, '')
   File.write(filename, new_html)
