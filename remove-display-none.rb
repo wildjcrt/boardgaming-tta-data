e800eab29f19340a8dd231ed381db63276a60918
wildjcrt <wildjcrt@yahoo.com.tw>
2018-05-06 10:10:02 +0800

    Remove display none in tta/*.html

--- remove-display-none.rb
+++ remove-display-none.rb
@@ -1,5 +1,5 @@
 Dir['tta/*'].each do |filename|
-  html = File.read(filename)
+  html = File.read(filename).encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
   new_html = html.gsub(/ style="display:none;"/, '')
   File.write(filename, new_html)
 end
